e687eafd3220e49de231129b4b65e3e5ca9e232a
IgorGabriel-Apps <77392705+IgorGabriel-Apps@users.noreply.github.com>
2021-02-14 21:10:06 -0300

    Add files via upload

---
+++ Aula02_Contas_Media.pas
@@ -0,0 +1,18 @@
+Program Aula02_Contas_Media;
+	 //Aplicativo que tira média
+var
+	Numero1:Integer;
+	Numero2:Integer;
+begin
+   Writeln('Digite o primeiro valor: ');
+   readln(Numero1);
+	 Writeln('Digite o segundo valor: ');
+   readln(Numero2);
+   clrscr;
+   Writeln('A média dos números é: ', ((Numero1 + Numero2) / 2):0:0);
+end.
+
+
+
+
+
